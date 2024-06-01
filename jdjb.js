c824c642d8f44cf63f4d4b81133df998de8591d8
liufsf <102776138+liufsf@users.noreply.github.com>
2024-06-02 01:05:16 +0800

    jdjb1.js

--- jdjb.js
+++ jdjb.js
@@ -8,7 +8,7 @@ const $ = new Env('京东价格保护');
 const unifiedGatewayName = 'https://api.m.jd.com'
 
 // 请先配置 token!!!最好抓APP的！
-let tokens = 'pt_key=AAJmW0j3ADBoz6nENW9rR-7hwc2wxTFwR1IXWB1JwFo5NDCekp3IqQ4kIYpnzEIVY7n7KWU63Us;pt_pin=jd_77397b6a47387;' // @ 分隔
+let tokens = 'pt_key=AAJmW0j3ADBoz6nENW9rR-7hwc2wxTFwR1IXWB1JwFo5NDCekp3IqQ4kIYpnzEIVY7n7KWU63Us;pt_pin=jd_77397b6a47387;@pt_key=AAJmW1FEADB-h-kjvQLwGvviRGPufl8YS0Tk0VPRzyek3DTahjlSpjaTmb0iXTqU1TFS42BKiP4;pt_pin=jd_7e62318b212a2;' // @ 分隔
 $.tokenList = []
 $.HyperParam = {
     sid_hid: '',
